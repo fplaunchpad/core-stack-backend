300dca4b21dcb57be51a4bc10e07f0965eb7c504
pawangramvaani <pawan.soni@gramvaani.org>
2026-05-22 03:55:31 -0700

    export csv data at GP level

--- plans/views.py
+++ plans/views.py
@@ -1095,10 +1095,7 @@ class OrganizationPlanViewSet(viewsets.ReadOnlyModelViewSet):
                     if user.organization != organization:
                         return PlanApp.objects.none()
                 queryset = PlanApp.objects.filter(
-                    organization=organization,
-                    enabled=True,
-                    is_dpr_reviewed=True,
-                    is_completed=True,
+                    organization=organization, enabled=True
                 )
             except Organization.DoesNotExist:
                 return PlanApp.objects.none()
@@ -1108,10 +1105,20 @@ class OrganizationPlanViewSet(viewsets.ReadOnlyModelViewSet):
         filter_test_demo = (
             self.request.query_params.get("filter_test_plan", "").lower() == "true"
         )
+        filter_dpr_reviewed = (
+            self.request.query_params.get("is_dpr_reviewed", "").lower() == "true"
+        )
+        filter_is_completed = (
+            self.request.query_params.get("is_completed", "").lower() == "true"
+        )
         if filter_test_demo:
             queryset = queryset.exclude(
                 Q(plan__icontains="test") | Q(plan__icontains="demo")
             )
+        if filter_dpr_reviewed:
+            queryset = queryset.filter(is_dpr_reviewed=True)
+        if filter_is_completed:
+            queryset = queryset.filter(is_completed=True)
 
         return queryset.order_by("-created_at")
 
